d9512c5f3c99552cbc90669fd82b523c9d1be240
EdyeMen <edye.mendez11@gmail.com>
2016-08-06 12:14:39 -0500

    realize controlador de robot

---
+++ application/logs/log-2016-07-23.php
@@ -0,0 +1,32 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+INFO - 2016-07-23 21:15:05 --> Config Class Initialized
+INFO - 2016-07-23 21:15:05 --> Hooks Class Initialized
+DEBUG - 2016-07-23 21:15:05 --> UTF-8 Support Enabled
+INFO - 2016-07-23 21:15:05 --> Utf8 Class Initialized
+INFO - 2016-07-23 21:15:05 --> URI Class Initialized
+DEBUG - 2016-07-23 21:15:05 --> No URI present. Default controller set.
+INFO - 2016-07-23 21:15:05 --> Router Class Initialized
+INFO - 2016-07-23 21:15:05 --> Output Class Initialized
+INFO - 2016-07-23 21:15:05 --> Security Class Initialized
+DEBUG - 2016-07-23 21:15:05 --> Global POST, GET and COOKIE data sanitized
+INFO - 2016-07-23 21:15:05 --> Input Class Initialized
+INFO - 2016-07-23 21:15:05 --> Language Class Initialized
+INFO - 2016-07-23 21:15:05 --> Loader Class Initialized
+INFO - 2016-07-23 21:15:05 --> Controller Class Initialized
+DEBUG - 2016-07-23 21:15:05 --> Config file loaded: D:\xampp\htdocs\ProyectoDiccionario\application\config/diccionario.php
+INFO - 2016-07-23 21:15:05 --> Helper loaded: form_helper
+INFO - 2016-07-23 21:15:05 --> Form Validation Class Initialized
+INFO - 2016-07-23 21:15:05 --> Helper loaded: url_helper
+INFO - 2016-07-23 21:15:05 --> Helper loaded: file_helper
+INFO - 2016-07-23 21:15:05 --> Helper loaded: security_helper
+INFO - 2016-07-23 21:15:05 --> Database Driver Class Initialized
+INFO - 2016-07-23 21:15:05 --> Database Utility Class Initialized
+ERROR - 2016-07-23 21:15:05 --> Language file contains no data: language/spanish/diccionario_message_lang.php
+INFO - 2016-07-23 21:15:05 --> File loaded: D:\xampp\htdocs\ProyectoDiccionario\application\views\system/head.php
+INFO - 2016-07-23 21:15:05 --> File loaded: D:\xampp\htdocs\ProyectoDiccionario\application\views\system/navbar.php
+INFO - 2016-07-23 21:15:05 --> File loaded: D:\xampp\htdocs\ProyectoDiccionario\application\views\administracion/panel.php
+INFO - 2016-07-23 21:15:05 --> File loaded: D:\xampp\htdocs\ProyectoDiccionario\application\views\system/footer.php
+INFO - 2016-07-23 21:15:05 --> File loaded: D:\xampp\htdocs\ProyectoDiccionario\application\views\system/layout.php
+INFO - 2016-07-23 21:15:05 --> Final output sent to browser
+DEBUG - 2016-07-23 21:15:05 --> Total execution time: 0.1630
